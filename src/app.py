2187f628848f4e72ca7499ec8ffb5f1ca60db7ee
filipelelis <fslelis@gmail.com>
2026-09-13 20:19:07 -0300

    fix: refatora testes para Node.js com Jest e Supertest seguindo a arquitetura

--- src/app.py
+++
@@ -1,8 +0,0 @@
-"""Ponto de entrada inicial da aplicação."""
-
-from .match import calcular_match
-
-def buscar_match(cachorro: dict, preferencia: dict) -> dict:
-    return {
-        "match": calcular_match(cachorro, preferencia)
-    }
